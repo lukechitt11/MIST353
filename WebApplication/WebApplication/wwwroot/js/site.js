1903dbf11c570796a91fdf296184679b6369201d
Luke C <142524788+lukechitt11@users.noreply.github.com>
2024-02-07 15:11:15 -0500

    Message

---
+++ WebApplication/WebApplication/wwwroot/js/site.js
@@ -0,0 +1,4 @@
+﻿// Add your custom JavaScript functionality here
+document.getElementById('customButton').addEventListener('click', function () {
+    alert('Button clicked! Custom JavaScript is working.');
+});
